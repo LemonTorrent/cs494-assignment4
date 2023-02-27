e95713680fca8c470be52306f130a81813c924b8
LemonTorrent <nyarbrough14@gmail.com>
2023-02-26 22:44:06 -0800

    removed comment

--- src/components/CandyList.js
+++ src/components/CandyList.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Candy from './Candy'
 
 function CandyList({ candies }) {
+    // console.log("Candies")
     return (
         <div>
             {candies.map(candy => <Candy key={candy.id} {...candy} />)}
