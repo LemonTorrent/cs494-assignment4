0e0539b6a5f80618053a3ad4d2f87abaffa155bc
LemonTorrent <nyarbrough14@gmail.com>
2023-02-26 22:46:40 -0800

    Added recducer for store products

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 
 import candyReducer from './candyReducer'
+import productReducer from './productReducer'
 // import visibilityFilterReducer from './visibilityFilterReducer'
 
 const rootReducer = combineReducers({
     candies: candyReducer,
     // visibilityFilter: visibilityFilterReducer
+    products: productReducer,
 })
 
 export default rootReducer
