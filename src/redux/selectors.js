c32784fe586bc612df7f73dbff050b3d3a5b3385
LemonTorrent <nyarbrough14@gmail.com>
2023-02-26 22:56:41 -0800

    Cleaned up file comments

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,15 +1,8 @@
 
 export function getCandies(state) {
-    console.log("Inside getCandies selectors, state: ", state)
     return state.candies
 }
 
 export function getProducts(state) {
-    console.log("Inside getProducts selectores, state: ", state)
-    // return (state.products ? [] : [])
-    //     console.log("Returning undefined")
-    // //     return null
-    // }
     return state.products
-    // return []
 }
