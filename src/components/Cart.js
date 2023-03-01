531086956dc5f5960b120e6ea5600ec64d9d4ce9
LemonTorrent <nyarbrough14@gmail.com>
2023-03-01 14:40:30 -0800

    implemented styling for cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,34 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {getCandies} from '../redux/selectors'
 import {removeCandy, receiveProduct, clearCandies} from '../redux/actions'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 
+const CartContents = styled.div`
+    // background: lightgray;
+    padding: 5px;
+
+`
+
+const RemoveButton = styled.button `
+    background-color: #e383a8;
+    color: #fff;
+    border: 2px solid #e383a8;
+    border-radius: 3px;
+    font-size: 12px;
+    font-weight: 300;
+    padding: 2px 5px;
+    margin: 5px;
+    cursor: pointer;
+`
+
+const CartListing = styled.div`
+    background: lightgrey;
+    p {
+        margin: 0;
+    }
+    margin: 10px 0;
+`
 
 function Cart({ candies }) {
     const dispatch = useDispatch()
@@ -15,35 +42,32 @@ function Cart({ candies }) {
     }
 
     return (
-        <div>
-            <div id="cart-contents">
-                <button onClick={()=>{
-                    console.log("cart contents:", cartContents)
-                    setShow(!show)
-                }}>{show ? "Hide" : "Show"} Cart ({cartContents.length})</button>
-                {
-                    show ?
-                        (
-                            <div>
-                                {cartContents.length > 0 ? cartContents.map(candy => {
-                                        return(<div>
-                                            <p>{candy.text}, {candy.quantity}, ${candy.quantity * candy.price}, id: {candy.id}</p>
-                                            <button onClick={()=>{
+        <CartContents id="cart-contents">
+            <button onClick={()=>{
+                console.log("cart contents:", cartContents)
+                setShow(!show)
+            }}>{show ? "Hide" : "Show"} Cart ({cartContents.length})</button>
+            {
+                show ?
+                    (
+                        <div>
+                            {cartContents.length > 0 ? cartContents.map(candy => {
+                                    return(
+                                        <CartListing>
+                                            <p>{candy.text}, {candy.quantity} pieces, ${candy.quantity * candy.price}</p>
+                                            <RemoveButton onClick={()=>{
                                                 removeFromCart(candy.id, candy.text, candy.quantity)
-                                                }}>Remove from Cart</button>
-                                        </div>
-                                        )
-                                    }) : <p>Nothing in Cart</p>}
-                                { cartContents.length > 0 ? <button onClick={()=>{
-                                    dispatch(clearCandies())
-                                }}>Check Out</button> : null }
-                            </div>
-                        ) : null
-                }
-            </div>
-            
-            
-        </div>
+                                                }}>Remove</RemoveButton>
+                                        </CartListing>
+                                    )
+                                }) : <p>Nothing in Cart</p>}
+                            { cartContents.length > 0 ? <button onClick={()=>{
+                                dispatch(clearCandies())
+                            }}>Check Out</button> : null }
+                        </div>
+                    ) : null
+            }
+        </CartContents>
     )
 }
 
